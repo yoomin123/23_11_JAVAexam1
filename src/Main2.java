2a124189edbfcf748ca03d39336773ad9501e38c
yoomin123 <yoominBB@gmail.com>
2023-11-11 10:27:05 +0900

    Scanner practice

---
+++ src/Main2.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main2 {
+  public static void main(String[] args) throws IOException {
+    System.out.println("===프로그램 시작===");
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String cmd = br.readLine();
+    System.out.printf("%s\n", cmd);
+    br.close();
+
+    System.out.println("===프로그램 종료===");
+  }
+}
---
+++ src/com/KoreaIT/java/AM/Main.java
@@ -0,0 +1,23 @@
+package com.KoreaIT.java.AM;
+// com파일 안에
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("===프로그램 시작===");
+
+    Scanner sc = new Scanner(System.in);
+    System.out.print("명령어 : ");
+    String cmd = sc.next(); // 입력하고 엔터 누르기 전까지 대기하고 있는 실행문
+    // next ==> 띄어쓰기 하기 전 한 단어만 입력됨.
+    // nextLine으로 입력된 것은 String 타입.
+    // nextint로 입력된 것은 int 타입.
+
+    System.out.println(cmd);
+
+    sc.close(); // 없어도 문법적으로 문제는 없지만 닫아주는 것이 좋음.
+
+    System.out.println("===프로그램 종료===");
+  }
+}
